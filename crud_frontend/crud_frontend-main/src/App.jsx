13b6184ce6f16510f7686566b1292973ab3cdf22
BoddigaSaikrishna <2300039074@kluniversity.in>
2025-08-20 01:12:03 +0530

    Update App.jsx

--- crud_frontend/crud_frontend-main/src/App.jsx
+++ crud_frontend/crud_frontend-main/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const handleChange = (e) => {
     setProduct({ ...product, [e.target.name]: e.target.value });
   };
-const BASE_URL = 'http://54.80.157.86/springapp1';
+const BASE_URL = 'http://54.80.157.86:9090/springapp1';
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
